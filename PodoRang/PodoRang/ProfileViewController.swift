5f1fa1905a394732b769fb0bb47ed3ff0d5d3397
Yeoju Seok <71719160+fhwmqkfl@users.noreply.github.com>
2023-05-24 23:54:31 +0900

    (add) : set image with pickerView, save user profile

--- PodoRang/PodoRang/ProfileViewController.swift
+++ PodoRang/PodoRang/ProfileViewController.swift
@@ -17,6 +17,8 @@ class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        setupImageView()
     }
     
     func setupUI() {
@@ -26,5 +28,52 @@ class ProfileViewController: UIViewController {
         profileImageView.layer.borderColor = UIColor.systemPurple.cgColor
         profileImageView.contentMode = .scaleAspectFill
         profileImageView.clipsToBounds = true
+        
+        saveButton.setTitle("SAVE", for: .normal)
     }
+    
+    func setupImageView() {
+        let tabGesture = UITapGestureRecognizer(target: self, action: #selector(test))
+        profileImageView.addGestureRecognizer(tabGesture)
+        profileImageView.isUserInteractionEnabled = true
+    }
+    
+    @objc func test() {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        self.present(picker, animated: true)
+    }
+    
+    @IBAction func saveButtonClicked(_ sender: UIButton) {
+        if let userimage = profileImageView.image, nameTextField.text != "" {
+            let user = UserData(username: nameTextField.text!, userImage: userimage)
+            UserData.list.append(user)
+            print(UserData.list)
+        } else {
+            print("유저명과 이미지를 다시한번 확인해주세요")
+        }
+    }
+}
+
+extension ProfileViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        dismiss(animated: true)
+        
+        let itemProvider = results.first?.itemProvider
+
+        if let itemProvider = itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self) {
+            itemProvider.loadObject(ofClass: UIImage.self) { (image, error) in
+                DispatchQueue.main.async {
+                    // 이미지 뷰에 표시
+                    self.profileImageView.image = image as? UIImage
+                }
+            }
+        } else {
+            print("이미지를 가져오는데 실패했습니다")
+        }
+    }
+    
 }
---
+++ PodoRang/PodoRang/UserData.swift
@@ -0,0 +1,15 @@
+//
+//  UserData.swift
+//  PodoRang
+//
+//  Created by coco on 2023/05/24.
+//
+
+import UIKit
+
+struct UserData {
+    var username: String
+    var userImage: UIImage
+    
+    static var list: [UserData] = []
+}
